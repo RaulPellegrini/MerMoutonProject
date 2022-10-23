eff7075921149d85bb4f9ff4f561230c03aaeb29
RaulPellegrini <raul_pellegrini@outlook.com>
2022-10-23 16:20:27 +0200

    She did it!

--- Assets/Scripts/AnotherMove.cs
+++ Assets/Scripts/AnotherMove.cs
@@ -8,11 +8,13 @@ public class AnotherMove : MonoBehaviour
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(-0.01f, 0, 0));
+        transform.Rotate(new Vector3(0, 0.08f, 0));
     }
 }
